f146425c85f08aa8e1a5157b69b4f43cefb76468
gornialberto <gorni.alberto@gmail.com>
2017-04-10 22:08:06 +0200

    some changes on the Program.cs

--- MyHome/Models/IFTTTActionData.cs
+++ MyHome/Models/IFTTTActionData.cs
@@ -15,6 +15,7 @@ namespace MyHomeAPI.Models
 
         public string ActionName { get; set; }
 
+        //to see if this can be http://www.newtonsoft.com/json/help/html/T_Newtonsoft_Json_Linq_JObject.htm
         public string ActionBodyContent { get; set; }
 
         public bool Processed { get; set; }
