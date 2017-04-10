b58f1341a3e07424a777b7b7bb42f4a62bcac3f4
gornialberto <gorni.alberto@gmail.com>
2017-04-10 17:38:24 +0200

    Update Program.cs
    no iis integration :)

--- MyHomeAPI/Program.cs
+++ MyHomeAPI/Program.cs
@@ -15,7 +15,7 @@ namespace MyHomeAPI
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+                //.UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
